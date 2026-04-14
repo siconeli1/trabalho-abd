6d663cc2e8cb0e27f593a6c1649264f8e1d12121
Lucas dos Santos Siconeli <151700777+siconeli1@users.noreply.github.com>
2026-04-14 08:07:54 -0300

    Create sequences and insert test data into tables
    Added sequences for generating IDs and inserted test data into category and language tables.

--- sequences.sql
+++ sequences.sql
@@ -1,10 +1,8 @@
 -- SEQUENCES UTILIZADAS EM TABELAS DO BANCO
 
--- -------------------------------------------
 -- 1. CRIAÇÃO DAS SEQUENCES
--- Objetivo: gerar IDs manualmente para inserts
--- em tabelas existentes do banco dvdrental
--- -------------------------------------------
+-- gerar IDs manualmente para inserts em tabelas existentes do banco dvdrental
+
 CREATE SEQUENCE seq_category_nova
 START WITH 1000
 INCREMENT BY 1;
@@ -14,11 +12,10 @@ START WITH 1000
 INCREMENT BY 1;
 
 
--- -------------------------------------------
+
 -- 2. INSERTS NA TABELA CATEGORY
--- Objetivo: utilizar a sequence criada em tabela
--- existente do banco
--- -------------------------------------------
+-- utilizar a sequence criada em tabela existente do banco
+
 INSERT INTO category (category_id, name)
 VALUES (nextval('seq_category_nova'), 'Categoria Teste 1');
 
@@ -26,11 +23,10 @@ INSERT INTO category (category_id, name)
 VALUES (nextval('seq_category_nova'), 'Categoria Teste 2');
 
 
--- -------------------------------------------
+
 -- 3. INSERTS NA TABELA LANGUAGE
--- Objetivo: utilizar a sequence criada em outra
--- tabela existente do banco
--- -------------------------------------------
+-- utilizar a sequence criada em outra tabela existente do banco
+
 INSERT INTO language (language_id, name)
 VALUES (nextval('seq_language_nova'), 'L1');
 
